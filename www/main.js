a11296cc3fbd2014c206e8d4eafedd885184d539
Sakuya Sugo <sakuya.s@gmail.com>
2016-05-24 20:05:22 +0900

    feature svg to canvas

--- www/main.js
+++ www/main.js
@@ -12,15 +12,26 @@ const app = {};
         constructor(svg) {
             this.svg = svg;
         }
+
         getSvg() {
             return this.svg;
         }
+
         getImage() {
             let img = new Image();
             img.src = base64dataURLencode((new XMLSerializer()).serializeToString(this.svg));
             return img;
         }
-        getCanvas() {
+
+        getCanvas(box = {width: 33, height: 33}) {
+            let img = this.getImage();
+            let canvas = document.createElement('canvas');
+            canvas.setAttribute("width", box.width);
+            canvas.setAttribute("height", box.height);
+            
+            let ctx = canvas.getContext('2d');
+            ctx.drawImage(img, 0, 0);
+            return canvas;
         }
     };
 
@@ -28,20 +39,36 @@ const app = {};
 
 (function (app) {
     "use strict";
-
-    let btn = document.getElementById("add").querySelector("a");
-    let images = document.querySelector(".images");
-    let svg = new app.MySVG(document.querySelector("svg"));
     
-    btn.addEventListener("click", (event) => {
+    let svg = new app.MySVG(document.querySelector("svg"));
+
+    // buttons
+    let add = document.getElementById("add").querySelector("a");
+    let cv = document.getElementById("canvas").querySelector("a");
+    
+    add.addEventListener("click", (event) => {
         event.preventDefault();
+        
+        let images = document.querySelector(".images");
         let img = images.querySelector("img");
+        
         if (img) {
             images.removeChild(img);
         }
         images.appendChild(svg.getImage());
+        
         console.log('added.');
     }, false);
+
+    cv.addEventListener("click", (event) => {
+        event.preventDefault();
+
+        let canvas = svg.getCanvas();
+
+        document.querySelector(".canvas").appendChild(canvas);
+
+        console.log("convert to canvas.");
+    });
     
     console.log('launched.');
 })(app);
